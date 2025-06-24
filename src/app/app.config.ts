39ac941771fddd122dc48849e66399c9cf845bdd
Juani8217 <you@example.com>
2025-06-23 22:22:46 -0300

    first commitprimer

---
+++ src/app/app.config.ts
@@ -0,0 +1,16 @@
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideHttpClient } from '@angular/common/http';  // <--- agregar esta importación
+
+import { routes } from './app.routes';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZonelessChangeDetection(),
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+    provideHttpClient()  // <--- agregar el provider aquí
+  ]
+};
